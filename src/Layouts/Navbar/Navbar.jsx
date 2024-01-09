9a6e30d790139fb21b7b267ac35d5c2896011bab
salomon.cjl <contrerassalomon6@gmail.com>
2024-01-08 23:23:52 -0300

    Layout navbar

---
+++ src/Layouts/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import './navbar.css'
+
+export default function Navbar() {
+  return (
+    <section className='cont-ul'>
+        <ul className='ul-navbar'>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Proyects</li>
+            <li>Contact</li>
+        </ul>
+    </section>
+  )
+}
