87eee4edc988f175b97baa0ce8978ada3bb2afeb
salomon.cjl <contrerassalomon6@gmail.com>
2024-01-15 23:59:45 -0300

    cambio

--- src/Layouts/Navbar/Navbar.jsx
+++ src/Layouts/Navbar/Navbar.jsx
@@ -4,10 +4,10 @@ export default function Navbar() {
   return (
     <section className='cont-ul'>
         <ul className='ul-navbar'>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Proyects</li>
-            <li>Contact</li>
+            <li className='li-titles'>&lt;About/&gt;</li>
+            <li className='li-titles'>&lt;Skills/&gt;</li>
+            <li className='li-titles'>&lt;Proyects/&gt;</li>
+            <li className='li-titles'>&lt;Contact/&gt;</li>
         </ul>
     </section>
   )
