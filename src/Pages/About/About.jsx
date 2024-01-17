cd69e1bebfa11b7ed5eab1957cfeb0efcc1d3de8
salomon.cjl <contrerassalomon6@gmail.com>
2024-01-16 23:59:18 -0300

    texto agregado al about

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,11 +1,15 @@
 import './about.css'
 
 export default function About() {
-  return (
-    <section className='cont-about'>
-        <h1 className='h1-presentacion'>Hola! soy Contreras Salomón, desarollador Full Stack!</h1>
-        <img src="vana_Cyberpunk.png" alt="" className='img-i'/>
+  return (<>
+      <section className='cont-about'>
+          <h1 className='h1-presentacion'>Hola! soy Contreras Salomón, desarollador Full Stack!</h1>
+          <img src="vana_Cyberpunk.png" alt="" className='img-i'/>
 
-    </section>
+      </section>
+      <section>
+        <p>Soy Desarollador Web, manejo el Stack MERN, titulado como Tecnico Superior en Programacion</p>
+      </section>
+    </>
   )
 }
